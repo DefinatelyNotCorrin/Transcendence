a177fe0df7faa535ab72982d1265d195f8d1295c
erebusmaster <Johnathan927@gmail.com>
2015-09-14 13:13:45 -0700

    added CardContainer class
    xml progress

---
+++ Transcendence/Assets/xml/CardContainer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+
+[XmlRoot("CardContainer")]
+public class CardContainer {
+        [XmlArray("Cards")]
+        [XmlArrayItem("Card")]
+        public List<Card> Cards = new List<Card>();
+}
+
+
+public static CardContainer Load(string path)
+{
+    var serializer = new XmlSerializer(typeof(CardContainer));
+    var stream = new FileStream(path, FileMode.Open);
+    var container = serializer.Deserialize(stream) as CardContainer;
+    stream.Close();
+}
