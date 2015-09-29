df1942f44d1c065f6216dd964ee187d778282717
RadiantCorrin <corbmod@gmail.com>
2015-09-28 20:35:10 -0700

    GM
    More GM work

--- Transcendence/Assets/scripts/GM.cs
+++ Transcendence/Assets/scripts/GM.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GM : MonoBehaviour {
+public class GM : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    //should create one single card from a list of cards, if ID and PATH for the XML file are known
+    public void generateCard(int cardID, string path)
+    {
+        //a list that will hold all the cards
+        List<Card> cards = new List<Card>();
+
+        //loads the cards into the list from an XML file
+        DeckReader reader = new DeckReader();
+        cards = reader.load(path);
+
+        //the card that will be put on the playing field
+        Card card;
+
+        //sets the in game card's values equal to the values it should have (gotten from the list<>)
+        card = cards[cardID];
+
+        //generates the card using the prefabCard prefab
+        card = (GameObject)Instantiate(prefabCard, transform.position, transform.rotation);
+
+    }
+
+    //should create cards for each one in a deck and then instanstiate them in the game
+    public void generateDeck(string path)
+    {
+
+        //all of the cards in a list<>
+        List<Card> cards = new List<Card>(); 
+
+        //loads the cards from the database or deck
+        DeckReader reader = new DeckReader();
+        cards = reader.load(path);
+
+        //the card that will be put on the playing field
+        Card card;
+
+        //information about the size of the array of cards, and what card the while loop is on
+        int totalCards = cards.Count;
+        int currentCard = 0;
+
+        //goes through the cards one bye one and adds them to the playing field
+        while (totalCards <= currentCard)
+        {
+            //sets the in game card's values equal to the values gotten from the list<>
+            card = cards[currentCard];
+
+            //generates the card using the card prefab
+            card = (GameObject)Instantiate(prefabCard, transform.position, transform.rotation);
+           
+            currentCard++;
+        }
+    }
 }
