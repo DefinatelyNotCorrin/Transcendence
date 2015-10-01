9f376e8b945f5c8b39a14b0a82507174c7fa68a7
erebusmaster <Johnathan927@gmail.com>
2015-10-01 13:14:58 -0700

    Deck class progress, card display test edits
    ^^

--- Transcendence/Assets/scripts/xml/CardDisplay.cs
+++ Transcendence/Assets/scripts/xml/CardDisplay.cs
@@ -11,6 +11,7 @@ public class CardDisplay : MonoBehaviour {
         path = Application.dataPath + "/scripts/xml/cards.xml";
         DeckReader reader = new DeckReader();
 
+
         if (File.Exists(path))
         {
             List<Card> database = reader.load(path);
@@ -19,10 +20,18 @@ public class CardDisplay : MonoBehaviour {
             {
                 print((card.name));
             }
+
+            Deck deck = new Deck();
+            deck.archiveDeck = database;
+            deck.activeDeck = database;
+            deck.resetActive();
+            Debug.Log(deck.poll());
+            deck.shuffle();
+            Debug.Log("2" + deck.poll());
         }
         else
         {
-            print(("Error: File not Found"));
+            Debug.Log("Error: File not Found");
         }
 	}
 
--- Transcendence/Assets/scripts/xml/Deck.cs
+++ Transcendence/Assets/scripts/xml/Deck.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -21,61 +20,61 @@ public class Deck : MonoBehaviour {
 	
 	}
 
-    int getArchiveSize() //returns size of deck before match started
+    public int getArchiveSize() //returns size of deck before match started
     {
         return archiveDeck.Count;
     }
 
-    int getDeckSize() //returns size of deck as it is, active, in the game
+    public int getDeckSize() //returns size of deck as it is, active, in the game
     {
         return activeDeck.Count;
     }
 
-    int getDiscardSize() //returns size of discard list
+    public int getDiscardSize() //returns size of discard list
     {
         return discard.Count;
     }
 
-    Card poll() //returns card of index 0 in deck, removes card from list
+    public Card poll() //returns card of index 0 in deck, removes card from list
     {
         Card card = activeDeck[0];
         activeDeck.RemoveAt(0);
         return card;
     }
 
-    Card poll(int i) //returns card of index i in deck, removes card from list
+    public Card poll(int i) //returns card of index i in deck, removes card from list
     {
         Card card = activeDeck[i];
         activeDeck.RemoveAt(i);
         return card;
     }
 
-    Card peek() //returns card of index 0 in deck, does not remove card
+    public Card peek() //returns card of index 0 in deck, does not remove card
     {
         return activeDeck[0];
     }
 
-    Card peek(int i) //returns card of index i in deck, does not remove card
+    public Card peek(int i) //returns card of index i in deck, does not remove card
     {
         return activeDeck[i];
     }
 
-    void insertCardToArchive(Card card, int index) //adds card to archive deck at specified index
+    public void insertCardToArchive(Card card, int index) //adds card to archive deck at specified index
     {
         archiveDeck.Insert(index, card);
     }
 
-    void insertCardToActive(Card card, int index) //adds card to active deck at specified index, deck top is 0
+    public void insertCardToActive(Card card, int index) //adds card to active deck at specified index, deck top is 0
     {
         activeDeck.Insert(index, card);
     }
 
-    void insertCardToDiscard(Card card, int index) //adds card to discard at specified index, discard top is 0
+    public void insertCardToDiscard(Card card, int index) //adds card to discard at specified index, discard top is 0
     {
         discard.Insert(index, card);
     }
 
-    void resetActive() //clears active Deck and sets it equal to archive
+    public void resetActive() //clears active Deck and sets it equal to archive
     {
         activeDeck.Clear();
         foreach (Card c in archiveDeck)
@@ -84,14 +83,14 @@ public class Deck : MonoBehaviour {
         }
     }
     
-    void shuffleDeck() {
-    //shuffling - Fisher-Yates method
-    float r = Random.range(0,activeDeck.Capacity());
-    int count = getDeckSize();
+    public void shuffle() { /*
+        //shuffling - Fisher-Yates method
+        int count = getDeckSize();
+        //float r = Random.Range(0,count);
         while (count > 1) //go through each element
         {
             //random number from 0 to range of unshuffled deck
-            int randomDraw = r.Next(0, count);
+            int randomDraw = (int)(Random.Range(0, count));
 
     //take whatever is drawn and swap it with the end of the list
     Card temp = activeDeck[randomDraw];
@@ -100,7 +99,7 @@ public class Deck : MonoBehaviour {
 
             count--;
 
-        }
+        } */
     }
 
     //SORTING,SHUFFLING,SERIALIZING
