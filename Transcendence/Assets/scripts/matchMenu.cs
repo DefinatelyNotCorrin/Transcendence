d4cc207dc5a60e2b7d06fc4ecf42b15584f224cb
RadiantCorrin <corbmod@gmail.com>
2015-10-06 13:16:15 -0700

    match

---
+++ Transcendence/Assets/scripts/matchMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class matchMenu : MonoBehaviour {
+
+
+    public Dropdown player1DeckSelector;
+    public Dropdown player2DeckSelecor;
+    public Button loadPlayer1Deck;
+    public Button loadPlayer2Deck;
+    public Dropdown.OptionData data = new Dropdown.OptionData();
+
+	// Use this for initialization
+	void Start () {
+	
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        data.text = "nerd";
+        player1DeckSelector.GetComponent<Dropdown>().options.Add(data);
+	
+	}
+}
