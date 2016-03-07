e2ba945f3de42f20b6e442977d471f6fd0c2c671
erebusmaster <Johnathan927@gmail.com>
2016-03-07 13:04:37 -0800

    Cardbook Update

--- Transcendence/Assets/scripts/DeckBuilder/CardBook.cs
+++ Transcendence/Assets/scripts/DeckBuilder/CardBook.cs
@@ -12,205 +12,30 @@ public class CardBook : MonoBehaviour
 	public GameObject spellCardPrefab;
 	//Reader
 	private string path;
-	private List<Card> activeDatabase;
-	private List<Card> archiveDatabase;
+	public Deck Database { get; set; } //all cards of database in selected alliance and not in current build
+	public Deck CurrentBuild { get; set; }//all cards in the deck the player is creating
 
-	// Use this for initialization
-	void Start()
+
+    void Start()
 	{
-
+		
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
 
-	}
-		
-
-	public int getArchiveSize() //returns size of database
-	{
-		return archiveDatabase.Count;
 	}		
+
+	public int GetArchiveSize() //returns size of database of all obtainable cards
+	{
+		return Database.archiveDeck.Count;
+	}	
+
+	public int GetCurrentBuildSize()
+	{
+        return Database.activeDeck.Count;
+	}
 		
-	public Card peek(int i) //returns card of index i in deck, does not remove card
-	{
-		return activeDeck[i];
-	}
-
-	public void insertCardToArchive(Card card, int index) //adds card to archive deck at specified index
-	{
-		archiveDeck.Insert(index, card);
-	}
-
-	public void insertCardToActive(Card card, int index) //adds card to active deck at specified index, deck top is 0
-	{
-		activeDeck.Insert(index, card);
-	}
-
-	public void insertCardToDiscard(Card card, int index) //adds card to discard at specified index, discard top is 0
-	{
-		discard.Insert(index, card);
-	}
-
-	public void resetActive() //clears active Deck and sets it equal to archive
-	{
-		activeDeck.Clear();
-		foreach (Card c in archiveDeck)
-		{
-			activeDeck.Add(c);
-		}
-	}
-
-	public void vacate() //clears Deck active and archive completely
-	{
-		activeDeck.Clear();
-		archiveDeck.Clear();
-		discard.Clear();
-		deckName = null;
-		path = null;
-
-	}
-
-	public void shuffle()
-	{ 
-		for (int i = activeDeck.Count - 1; i > 0; i--) {
-			int r = UnityEngine.Random.Range(0,i);
-			Card tmp = activeDeck[i];
-			activeDeck[i] = activeDeck[r];
-			activeDeck[r] = tmp;
-		}
-	}
-
-	public List<Card> filterName(string name)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.cardName.Equals(name, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
-	public List<Card> filterID(string ID)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.ID.Equals(ID, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
-	public List<Card> filterAlliance(string alliance)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.alliance.Equals(alliance, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
-	public List<Card> filterType(string type)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.type.Equals(type, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
-	public List<Card> filterCost(string cost)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.cost.Equals(cost, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
-	public List<Card> filterAttack(string attack)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.attack.Equals(attack, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
-	public List<Card> filterHealth(string health)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.health.Equals(health, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
-	public List<Card> filterDefense(string defense)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.defense.Equals(defense, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
-	public List<Card> filterRange(string range)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.range.Equals(range, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
-	public List<Card> filterTarget(string target)
-	{
-		List<Card> filtered = new List<Card>();
-		foreach (Card c in activeDeck)
-		{
-			if (c.target.Equals(target, StringComparison.InvariantCultureIgnoreCase))
-			{
-				filtered.Add(c);
-			}
-		}
-		return filtered;
-	}
-
 }
 
